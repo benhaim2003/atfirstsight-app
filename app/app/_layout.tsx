c1c59441880653d2f7ce566d5ee51275fe8b2e2a
galsavu <galsavu@gmail.com>
2025-12-13 19:47:55 +0200

    Created basic different states layouts

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,10 +1,20 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import { UserState, UserAuthState } from "@/types/user";
 import { useUserStore } from "@/src/store/useUserStore";
 
 
 export default function RootLayout() {
   const userAuthState = useUserStore((state) => state.userAuthState);
+
+  if (userAuthState === UserAuthState.Guest)
+  {
+    return <Redirect href="./(auth)/signIn" />;
+  }
+
+  if (userAuthState === UserAuthState.User)
+  {
+    return <Redirect href="./(auth)/profileCreation" />;
+  }
   
   return (
       <Stack>
