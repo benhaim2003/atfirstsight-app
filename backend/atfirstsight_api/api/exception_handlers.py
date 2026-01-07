23dd3754fc8b604691df4e0d83aebf37ed76489b
benhaim2003 <benhaim1254@gmail.com>
2026-01-07 21:45:21 +0200

    adding db_exception_handler

--- backend/atfirstsight_api/api/exception_handlers.py
+++ backend/atfirstsight_api/api/exception_handlers.py
@@ -1,7 +1,12 @@
 from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse
 
-from atfirstsight_api.db.exceptions import ItemNotFoundException, DuplicateItemException, AccessDeniedException
+from atfirstsight_api.db.exceptions import ItemNotFoundException, DuplicateItemException, AccessDeniedException, \
+    DBException
+
+
+async def db_exception_handler(_: Request, exc: DBException) -> JSONResponse:
+    return JSONResponse(status_code=500, content={"detail": str(exc)})
 
 
 async def item_not_found_exception_handler(_: Request, exc: ItemNotFoundException) -> JSONResponse:
@@ -17,6 +22,7 @@ async def duplicate_item_exception_handler(_: Request, exc: DuplicateItemExcepti
 
 
 def add_exception_handlers(app: FastAPI) -> None:
+    app.add_exception_handler(DBException, db_exception_handler)
     app.add_exception_handler(ItemNotFoundException, item_not_found_exception_handler)
     app.add_exception_handler(AccessDeniedException, access_denied_exception_handler)
     app.add_exception_handler(DuplicateItemException, duplicate_item_exception_handler)
